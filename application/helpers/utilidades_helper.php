0d7d3e8230dbbd739c635925393d923aa89dc3f7
fbarrero58 <felipe.barrero@valormaximo.com>
2018-03-05 10:10:16 -0300

    Base del proyecto - Utilizar para empezar con la estructura básica

---
+++ application/helpers/utilidades_helper.php
@@ -0,0 +1,48 @@
+<?php
+
+    function capitalizar_arreglo( $data_cruda, $campos_capitalizar, $todos = FALSE ){
+
+        $data_lista = $data_cruda;
+
+        foreach( $data_cruda as $nombre_campo => $valor_campo ){
+
+            // echo 'Nombre campo: ' . $nombre_campo.'<br>';
+            // echo 'Valor campo: ' . $valor_campo.'<br>';
+            // echo '------------------<br>';
+
+            if( in_array( $nombre_campo, array_values($campos_capitalizar) ) || $todos ){
+                $data_lista[ $nombre_campo ] = strtoupper($valor_campo);
+            }
+
+        }
+
+        return $data_lista;
+
+    }
+
+    function obtener_mes( $mes ){
+
+        $meses = array (
+            'enero',
+            'febrero',
+            'marzo',
+            'abril',
+            'mayo',
+            'junio',
+            'julio',
+            'agosto',
+            'septiembre',
+            'octubre',
+            'noviembre',
+            'diciembre'
+        );
+
+        return $meses[$mes];
+
+    }
+
+    function obtener_anio(){
+
+    }
+
+?>
